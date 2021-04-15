b3b19f79c5176ef3658c200cb2b3a84cbd6c9bc7
Arasakumar Nachimuthu <arasakumarn@histogenetics.com>
2021-04-15 22:54:10 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+btnRtrim = () => {
+    const data = document.getElementById('originalData').value.split('\n');
+    let strFormattedData = '';
+    for (var i = 0; i < data.length; i++) {
+        strFormattedData = strFormattedData + data[i].trimEnd() + '\n';
+    }
+    document.getElementById('modifiedData').value = strFormattedData;
+};
+
+copyModifiedText = () => {
+    const modTxtArea = document.getElementById('modifiedData');
+    modTxtArea.focus();
+    modTxtArea.select();
+    alert(document.execCommand('copy'));
+};
